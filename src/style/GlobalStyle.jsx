e8f5a6038bb6438fafd19f10126092fbd70c4d94
podoDJ <ourstar1345@gmail.com>
2023-06-26 20:23:19 +0900

    basic color added

--- src/style/GlobalStyle.jsx
+++ src/style/GlobalStyle.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function GlobalStyle() {
   return (
     <div>
-      
+{/* :root {
+--color-bg : #FFF4E0
+--color-text : #4D4D4D
+--color-accent : #B46060
+--color-box : #FFBF9B
+} */}
     </div>
   )
 }
