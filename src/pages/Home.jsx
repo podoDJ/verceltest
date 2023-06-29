23c14654638918c503a1e26613f9f587abb73b79
Lee Jinsol <jins2ol@naver.com>
2023-06-29 20:06:43 +0900

    Fix/ Resolve issue with popularity posts sorting not working correctly
    포스트 좋아요 순으로 정렬이 안되는 문제 해결
    + 메인 페이지 스타일 추가
    + 헤더 아이콘 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import HomeComp from "../components/home/HomeComp";
-import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Home = () => {
   const currentUid = useSelector((state) => state.logReducer.user.uid);
-  console.log("currentUid =>", currentUid)
+  console.log("currentUid =>", currentUid);
   return (
     <>
-      <div>홈 화면입니다.</div>
       <HomeComp />
-      <Link to={"/post"}>전체게시글로</Link>
     </>
   );
 };
